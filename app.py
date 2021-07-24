e9ee8f262f1709adbb72a41e6215d8b2312f0104
Hamza <rafi.hamza2020@gmail.com>
2021-07-24 14:23:41 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, redirect, session, request, jsonify
+from pymongo import MongoClient
+import hashlib
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+client = MongoClient('mongodb://localhost:27017/')
+
+db = client.userAccountSystemDatabase
+userCollection = db.users
+
+@app.route('/')
+def index():
+    if 'isLoggedIn' in session:
+        return render_template('index.html', user=session['user'])
+    else:
+        return redirect('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "GET":
+        return render_template('login.html')
+    else:
+        accountDetails = {}
+
+        accountDetails['password'] = request.form['password'].strip()
+        accountDetails['username'] = request.form['username'].strip()
+
+        hashedPassword = hashlib.sha256(accountDetails['password'].encode('utf-8')).hexdigest()
+
+
+        user = userCollection.find_one({'username': accountDetails['username'],'hashedPassword': hashedPassword})
+        if user:
+            # successful log in
+            session['isLoggedIn'] = True
+            session['user'] = {
+                'userName': user['username'],
+                'name': user['name']
+            }
+            return redirect('/')
+        else:
+            return jsonify({'error': 'Login Details incorrect. Please try again'})
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == "GET":
+        return render_template('createAccount.html')
+    else:
+        accountDetails = request.form
+        hashedPassword = hashlib.sha256(accountDetails['password'].encode('utf-8')).hexdigest()
+
+        userDetails = {
+            'name': accountDetails['name'],
+            'username': accountDetails['username'],
+            'hashedPassword': hashedPassword
+        }
+        userCollection.insert_one(userDetails)
+        return redirect('/login')
+
+@app.route('/delete')
+def deleteDatabase():
+    client.drop_database('userAccountSystemDatabase')
+    return 'Deleted Databse'
